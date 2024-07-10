c060791cae0623796d922159f4ac71babb8bc4ec
mdb <734436664@qq.com>
2024-07-11 00:31:39 +0800

    feat: 引入 Tailwind CSS

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div class="mb-4">
+    <div class="container mx-auto">
       <el-button>Default</el-button>
       <el-button type="primary">Primary</el-button>
       <el-button type="success">Success</el-button>
       <el-button type="info">Info</el-button>
       <el-button type="warning">Warning</el-button>
       <el-button type="danger">Danger</el-button>
+    </div>
+    <div class="flex flex-row">
+      <div class="basis-1/4">01</div>
+      <div class="basis-1/4">02</div>
+      <div class="basis-1/2">03</div>
     </div>
   </div>
 </template>
