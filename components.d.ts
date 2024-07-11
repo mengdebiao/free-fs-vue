27ce34e2e2aa19f849fc1bb824689a8e871c9cf4
mdb <734436664@qq.com>
2024-07-11 23:42:32 +0800

    feat: 清理无用文件

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/base.css'
 import './styles/index.css'
 
 import { createApp } from 'vue'
